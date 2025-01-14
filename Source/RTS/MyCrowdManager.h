522468e97bc63920af7ffa1c088ac5be5817872b
mateo809 <150682933+mateo809@users.noreply.github.com>
2025-01-14 12:29:09 +0100

    Game

---
+++ Source/RTS/MyCrowdManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navigation/CrowdManager.h"
+#include "MyCrowdManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_API UMyCrowdManager : public UCrowdManager
+{
+	GENERATED_BODY()
+	
+};
